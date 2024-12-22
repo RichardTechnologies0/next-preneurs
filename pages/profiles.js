3e48d3bf7ca9b5f6affb1e559abe047ec5976c83
RichardTechnologies0 <richardfrancoisjean0@gmail.com>
2024-12-22 14:37:32 -0500

    Update profiles.js

--- pages/profiles.js
+++ pages/profiles.js
@@ -1,4 +1,5 @@
 
+
 import { useEffect, useState } from 'react';
 import { useMyContext } from '@/context/MyContext';
 export default function ProfilePage() {
@@ -8,6 +9,11 @@ export default function ProfilePage() {
   const [newNom,setNom]=useState('');
   const [newUrl,setUrl]=useState('');
   const [newOccupation,setOccupation]=useState('');
+  const [isMenuOpen,setIsMenuOpen]=useState(false);
+
+  function toggleMenu(){
+    setIsMenuOpen(!isMenuOpen);
+  }
 
   useEffect(()=>{
     setEmail(email);
@@ -17,9 +23,10 @@ export default function ProfilePage() {
   });
 
   return (
-    <div className="bg-white mx-auto mt-32 flex flex-col sm:flex-row justify-center items-center shadow-lg rounded-lg p-6 max-w-lg">
+    <div >
+    <div className="grid lg:grid-cols-4  sm:flex-row md:grid-cols-2  gap-4 bg-white mx-auto lg:mt-32 flex lg:flex-col justify-center items-center lg:shadow-lg rounded-lg p-6 max-w-50">
       {/* Image de profil */}
-      <div className="mb-4 sm:mb-0 sm:mr-4">
+      <div className="mb-4 flex justify-center items-center  sm:mx-auto">
         <img
           src={newUrl || "https://via.placeholder.com/150"}
           alt="Photo de profil"
@@ -34,7 +41,48 @@ export default function ProfilePage() {
         </h4>
         <p className="text-sm text-gray-500">{newEmail || "Email non spécifié"}</p>
         <p className="text-sm text-gray-500">{newOccupation || "Occupation non spécifiée"}</p>
+        <p className='text-sm text-blue-400'>Investisseur</p>
+
       </div>
+
+      <div className=''>
+          <p className='text-gray-500 text-sm'>Objectifs de l'entrepreneur avec son statut </p>
+      </div>
+    </div>
+    <div className='flex justify-center m-2 p-2 items-center' onClick={toggleMenu}>
+            <button className='text-blue-500 md:hidden'>
+              <i className='text-lg fa fa-bars'></i>
+            </button>
+    </div>
+    <nav className='hidden md:flex justify-center divide-x divide-blue-500 items-center space-x-4'>
+               <a className='text-blue-500 hover:text-green-700 font-bold ' href='#'>Projets Realises</a>
+               <a className='text-blue-500 hover:text-green-700 font-bold ml-lg' href='#'>Etudes de cas</a>
+               <a className='text-blue-500 hover:text-green-700 font-bold ml-lg' href='#'>Certifications</a>
+               <a className='text-blue-500 hover:text-green-700 font-bold ml-lg' href='#'>Evenements / Webinaires</a>
+               <a className='text-blue-500 hover:text-green-700 font-bold ml-lg' href='./chatIA'>ChatIA</a>
+               <a href="./newPost" className="hover:text-gray-300 rounded-full border ml-lg outline outline-offset-2 text-blue-500 m-2 p-2 border-blue-500">
+             <i className="fa fa-plus"></i>Add Post
+           </a>
+           </nav>
+    <div >
+      {isMenuOpen &&(
+               <nav className='flex flex-col items-center space-y-4'>
+               <a className='text-blue-500 hover:text-green-700 font-bold ' href='#'>Projets Realises</a>
+               <a className='text-blue-500 hover:text-green-700 font-bold ' href='#'>Etudes de cas</a>
+               <a className='text-blue-500 hover:text-green-700 font-bold mx-auto' href='#'>Certifications</a>
+               <a className='text-blue-500 hover:text-green-700 font-bold ' href='#'>Evenements / Webinaires</a>
+               <a href="./newPost" className="hover:text-gray-300 rounded-full border outline outline-offset-2 text-blue-500 m-2 p-2 border-blue-500">
+             <i className="fa fa-plus"></i>Add Post
+           </a>
+           </nav>
+      )}
+    </div>
+  
+      <div className='mx-auto shadow-xl max-w-lg flex justify-center items-center'>
+        <input type='text' placeholder='Rechercher' className='p-2 m-2 border border-gray' /> 
+        <button className='m-4 p-2 bg-blue-500 hover:bg-blue-800 text-white rounded-full'><i className='fa fa-search m-2'></i>Rechercher</button>
+      </div>
+      
     </div>
   );
 }
